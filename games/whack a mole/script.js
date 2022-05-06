c8eec97a31374d3f4ff84ab7a48d86844eb24c8a
tinegaCollins <collinstinega003@gmail.com>
2022-05-06 15:35:28 +0300

    whack a mode done

--- games/whack a mole/script.js
+++ games/whack a mole/script.js
@@ -1,7 +1,16 @@
 let divs = [...document.querySelectorAll("div")]
 setInterval(() => {
-    let num = Math.floor(Math.random(0,8.4)*10)
+    let num = Math.floor(Math.random(0,7)*10)
     console.log(num)
     divs[num].classList.add("red")
-    
+    setTimeout(() => {
+        divs[num].classList.remove("red") 
+    }, 1000);
 },1000);
+for (let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener("click",()=>{
+        if(divs[i].has("red")){
+            console.log("whacked")
+        }
+    })
+}
