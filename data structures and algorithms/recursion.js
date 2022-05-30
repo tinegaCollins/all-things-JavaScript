8c908d6a466a310585d455be249d26f20b945376
tinega collins <collinstinega003@gmail.com>
2022-05-30 13:16:19 +0000

    last

--- data structures and algorithms/recursion.js
+++ data structures and algorithms/recursion.js
@@ -1,18 +1,22 @@
-function getSecondLetter(a,b,c){
-    if(b[c] == a){
-        return b.indexOf(b[c])
+function getSecondLetter(letter,,starting){
+    let indexOfSecondLetter;
+    if(word[starting] == letter){
+        indexOfSecondLetter = word.indexOf(word[startingPoint])
+        return(indexOfSecondLetter,indexOfFirstLetter)
+    }
+    else {
+        return getSecondLetter(letter,word,startingPoint += 1)
     }
 }
 
 function getLetter(letter1,letter2,word,startingPoint){
     let indexOfFirstLetter;
-    let indexOfSecondLetter;
     if (word[startingPoint] == letter1){
         indexOfFirstLetter = word.indexOf(word[startingPoint])
         getSecondLetter(letter2,word,startingPoint)
     }
     else {
-        return getLetter(letter1,letter2,word,startingPoint + 1)
+        return getLetter(letter1,word,startingPoint += 1)
     }
 }
 console.log(getLetter('a','b',['e','r','a','t','b'], 0))
