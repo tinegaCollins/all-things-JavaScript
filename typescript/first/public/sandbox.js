f78974a60ab962275d962c4d2d8207a9130b2f28
tinegaCollins <collinstinega003@gmail.com>
2022-06-27 20:54:09 +0300

    done with tyescript today

---
+++ typescript/first/public/sandbox.js
@@ -0,0 +1,64 @@
+"use strict";
+let names = ['lui', 'mario', 'cole'];
+let numbers = [2, 4, 6, 6];
+numbers.push(5);
+let mixed = ["ken", 'chun-li', 9, 0];
+mixed.push("rtr");
+mixed.push(4);
+let ninja = {
+    name: 'mario',
+    belt: true,
+    age: 40
+};
+ninja = {
+    name: 'mariogg',
+    belt: false,
+    age: 40
+};
+let character;
+let age;
+let isLogged;
+character = "tinega";
+let icons = [];
+icons.push("fhfj");
+const add = (num, num2) => {
+    return num + num2;
+};
+let myAge = 45;
+myAge = "new age ";
+myAge = {
+    age: 22,
+    name: "collins"
+};
+console.log(myAge);
+let mixedArray = [];
+mixedArray.push("tinega");
+mixedArray.push(7);
+mixedArray.push(true);
+console.log(mixedArray);
+let ninjaObject;
+ninjaObject = {
+    name: "string",
+    age: true
+};
+let anyNinja;
+anyNinja = {
+    name: 5,
+    age: "gut"
+};
+console.log("compilled");
+let dead;
+dead = true;
+let great = () => {
+    console.log("hello");
+};
+const addNumbers = (a, b, c = 10) => {
+    console.log(a + b + c);
+    console.log(c);
+};
+addNumbers(2, 3, 4);
+const minus = (a, b) => {
+    return a - b;
+};
+const result = minus(2, 4);
+console.log(result);
---
+++ typescript/first/src/sandbox.ts
@@ -0,0 +1,82 @@
+let names = ['lui', 'mario', 'cole'];
+
+let numbers = [2,4,6,6];
+numbers.push(5);
+
+let mixed = ["ken", 'chun-li', 9, 0];
+
+mixed.push("rtr");
+mixed.push(4);
+
+
+let ninja = {
+    name: 'mario',
+    belt : true,
+    age: 40
+}
+ninja = {
+    name: 'mariogg',
+    belt : false,
+    age: 40
+}
+
+let character: string;
+let age:number;
+let isLogged: boolean;
+
+character = "tinega";
+
+
+let icons: string[] = [];
+icons.push("fhfj")
+
+const add = (num:number, num2:number)=>{
+    return num + num2
+}
+
+let myAge:any = 45;
+myAge= "new age ";
+myAge = {
+    age: 22,
+    name: "collins"
+}
+console.log(myAge);
+
+let mixedArray: any[] = [];
+
+mixedArray.push("tinega")
+mixedArray.push(7)
+mixedArray.push(true)
+
+console.log(mixedArray);
+
+let ninjaObject: {name:string,age:boolean};
+ninjaObject = {
+    name: "string",
+    age: true
+}
+let anyNinja: { name: any, age: any}
+anyNinja= {
+    name: 5,
+    age: "gut"
+}
+console.log("compilled")
+
+let dead:boolean;
+dead = true;
+
+let great = ()=>{
+    console.log("hello");
+}
+
+const addNumbers = (a:number,b:number, c: number = 10) =>  {
+    console.log(a+b+c);
+    console.log(c)
+}
+addNumbers(2,3,4)
+
+const minus = (a:number,b:number): number =>{
+    return a - b;
+}
+const result = minus(2,4);
+console.log(result);
