3b3fae7663b1b515ff8546c64065b1d4318adbb2
tinegaCollins <collinstinega003@gmail.com>
2022-07-04 10:55:08 +0300

    charts done

--- charts.Js/example1/src/App.vue
+++ charts.Js/example1/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <BarChart />
+  <barchart2Vue/>
 </template>
 
 <script>
 import BarChart from './components/BarChart.vue'
+import barchart2Vue from './components/barchart2.vue'
+
 
 export default {
   name: 'App',
-  components: { BarChart }
+  components: { BarChart, barchart2Vue }
 }
 </script>
---
+++ charts.Js/example1/src/components/barchart2.vue
@@ -0,0 +1,41 @@
+<template>
+    <Line :chart-data="data" :chart-options="options" css-classes="chart-container"/>
+</template>
+
+<script setup>
+import {ref,computed} from 'vue';
+import { Line } from 'vue-chartjs'
+
+import { Chart, LineController, PointElement, LineElement } from 'chart.js'
+
+Chart.register(LineController,LineElement, PointElement);
+
+const dataValues = ref([12,13,3,5,16,17,18])
+
+const data = computed(()=> ({
+    labels : ["sun","mon","tue","wed","thurs","fri","sat"],
+
+    datasets: [
+        {
+            label: 'Temperature',
+            backgroundColor: 'blue',
+            data: dataValues.value
+        }
+    ]
+}))
+
+const options  = ref({
+    plugins: {
+        title: {
+            text: 'Temperature and Humidity',
+        }
+    },
+})
+</script>
+
+<style> 
+.chart-container {
+    width: 300px;
+    height: 300px;
+}
+</style>
